d5eaaedcc885696852708b7f1cf1e1a07f9d6fe3
vlad.shuisky208 <vlad.shuisky@gmail.com>
2024-11-24 13:03:20 +0300

    kaef

--- config.py
+++ config.py
@@ -1,14 +1,21 @@
-env: dict = {}
+# env: dict = {}
 
-with open('env.txt', 'r') as envfile:
-    env = {
-        var_str.split('=')[0]: var_str.split('=')[1] 
-        for var_str 
-        in envfile.read().split('\n')
-    }
+# with open('env.txt', 'r') as envfile:
+#     env = {
+#         var_str.split('=')[0]: var_str.split('=')[1] 
+#         for var_str 
+#         in envfile.read().split('\n')
+#     }
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.environ)
 
 try:
-    TOKEN = env.get('TOKEN')
-    BOT_USERNAME = env.get('BOT_USERNAME')
+    TOKEN = os.environ.get('TOKEN')
+    BOT_USERNAME = os.environ.get('BOT_USERNAME')
 except KeyError:
     print('env.txt файл не содержит данных переменных')
